e1a047de59b64d65765237ea8780485826b30dfa
kimjjunho <kimjjun1007@gmail.com>
2022-07-20 20:04:25 +0900

    gitflow-feature-stash: satp_11_fix_login

--- app/src/main/java/com/saehyun/myapplication/LoginActivity.kt
+++ app/src/main/java/com/saehyun/myapplication/LoginActivity.kt
@@ -2,10 +2,18 @@ package com.saehyun.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btn : Button = findViewById(R.id.btn)
+
+        btn.setOnClickListener {
+            Toast.makeText(applicationContext, "gitflow연습", Toast.LENGTH_SHORT).show()
+        }
     }
 }
