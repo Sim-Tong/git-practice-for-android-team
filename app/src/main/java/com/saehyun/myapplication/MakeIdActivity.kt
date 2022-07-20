658268a1676d4545c743cd3c38cc267d0995ac81
kimjjunho <kimjjun1007@gmail.com>
2022-07-20 20:25:36 +0900

    feat: make_id_ui

---
+++ app/src/main/java/com/saehyun/myapplication/MakeIdActivity.kt
@@ -0,0 +1,11 @@
+package com.saehyun.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MakeIdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_make_id)
+    }
+}
